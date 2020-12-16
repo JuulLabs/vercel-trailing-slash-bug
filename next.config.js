7dabab6f951b4e115ece4c9a13c4ac742c9b99d4
Jesse Collier <jessecollier@gmail.com>
2020-12-16 17:28:44 -0500

    ensuring rewrites are enabled

--- next.config.js
+++ next.config.js
@@ -11,24 +11,16 @@ const rewrites = []
 for (const store in stores) {
   const { locales } = stores[store]
 
-  // Homepage
-  for (const locale of locales) {
-    rewrites.push({
-      destination: `/${store}/${locale}/home`,
-      source: `/${store}/${locale}`,
-    })
-  }
-
   // Rerewrites for single-locale stores, to allow their locale to be omitted
   // from URL params
-  if (locales.length === 1) {
+  //if (locales.length === 1) {
     rewrites.push(...[
       {
         destination: `/${store}/${locales[0]}/:path*`,
         source: `/${store}/:path*`,
       },
     ])
-  }
+  //}
 }
 
 module.exports = {
